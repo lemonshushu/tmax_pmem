29492a6f37b4b08aec4863f8b3ae7adf4394dc31
Subin Song <subin_song@tmax.co.kr>
2022-07-19 16:30:37 +0900

    add some lines to example

--- example1.c
+++ example1.c
@@ -10,18 +10,24 @@ int main()
 {
     const char *dir = "/pmem/tmp/";
     const char *str1 = "Hello World";
+    const char *str2 = "Changed String";
     struct pmem_file *pfile;
     void *addr;
-    addr = request_pmem(dir, NULL, strlen(str1) + 1, &pfile);
+    addr = request_pmem(dir, NULL, 100 * sizeof(char), &pfile);
     if (addr == NULL)
     {
         printf("[%s] request_pmem failed\n", __func__);
         goto exit;
     }
-    strcpy(addr, str1);
 
-    // Print string inside addr
+    strcpy(addr, str1);
     printf("[%s] addr: %s\n", __func__, (char *)addr);
+
+    // Change string inside addr
+    strcpy(addr, str2);
+    printf("[%s] addr: %s\n", __func__, (char *)addr);
+
+
     pmem_cleanup(addr, &pfile);
     return 0;
 exit:
